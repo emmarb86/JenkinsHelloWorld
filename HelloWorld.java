0f4fc73d171e0c37e09ada603b59cfeb99271824
emmarb86 <emmarbustos@gmail.com>
2024-01-31 12:27:36 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World, Jenkins training with some changes for webhook!"); 
+        System.out.println("Hello World, made some changes for webhook!"); 
     }
 }
