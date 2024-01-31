05462d25ae8d35fb1ad4fc54162ea80373ba2120
emmarb86 <emmarbustos@gmail.com>
2024-01-31 12:48:09 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World, made some changes for webhook from pc!"); 
+        System.out.println("Hello World!"); 
+        System.out.println("made some changes for webhook!"); 
     }
 }
